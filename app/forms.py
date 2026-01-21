98ce7e106a24b9614b6f3d37c3b1cbfbb08da6cc
arumes31 <114224498+arumes31@users.noreply.github.com>
2026-01-21 21:23:05 +0100

    UI: Rename Login button from Unlock to Login

--- app/forms.py
+++ app/forms.py
@@ -35,7 +35,7 @@ class BulkUploadForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Username or Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Unlock')
+    submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
